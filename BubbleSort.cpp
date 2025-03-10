60d4c1a7152d7ae17fc2c415f369beb9ec357f00
Rohit Desale <160694649+DesaleRohit@users.noreply.github.com>
2025-03-10 09:08:26 +0530

    [MODIFIED]:Rohit - DS program for Bubble Sort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,42 +1,35 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
-int main()
-{
+
+int main() {
     int size;
-    cout << "Enter the Size : ";
+    cout << "Enter the Size: ";
     cin >> size;
     int a[size];
 
-    cout << "Enter the Elements : ";
-    for (int i = 0; i < size; i++)
-    {
+    cout << "Enter the Elements: ";
+    for (int i = 0; i < size; i++) {
         cin >> a[i];
     }
 
-    cout << "Befor Sorting : ";
-    for (int i = 0; i < size; i++)
-    {
+    cout << "Before Sorting: ";
+    for (int i = 0; i < size; i++) {
         cout << a[i] << "  ";
     }
     cout << endl;
 
-    for (int i = 0; i < size; i++)
-    {
-        for (int k = 0; k < size; k++)
-        {
-            if (a[i] < a[k])
-            {
-                int temp = a[i];
-                a[i] = a[k];
-                a[k] = temp;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (a[j] > a[j + 1]) { 
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
         }
     }
 
-    cout << "After Sorting : ";
-    for (int i = 0; i < size; i++)
-    {
+    cout << "After Sorting: ";
+    for (int i = 0; i < size; i++) {
         cout << a[i] << "  ";
     }
     return 0;
